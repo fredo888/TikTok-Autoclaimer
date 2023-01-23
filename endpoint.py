b1018748bb73d806ff481a88d67fb6bc273f1f37
fredo888 <54877359+fredo888@users.noreply.github.com>
2023-01-23 11:17:36 -0600

    Create endpoint.py

---
+++ endpoint.py
@@ -0,0 +1,6 @@
+https://webcast16-normal-useast5.us.tiktokv.com/webcast/room/mget_user_info?channel=googleplay&device_type=tt&os_version=8.0.0&version_code=160504&app_name=musical_ly&device_platform=android&aid=1180&source=mofang&sec_user_ids=
+
+this tiktok endpoint has no rate limit, you can check up to 51 users or, you can single check and hit up to 80krs!.
+this was even tested on a 8 core server.
+
+you can use this endpoint to autoclaim very amazing usernames!

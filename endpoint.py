f4d3d6ed9f0156ff96894ee75ac408c5036dddff
fredo888 <54877359+fredo888@users.noreply.github.com>
2023-01-23 11:25:09 -0600

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,4 +1,4 @@
-curl --location --request POST 'https://webcast16-normal-useast5.us.tiktokv.com/webcast/room/mget_user_info?sec_user_ids=MS4wLjABAAAAZAE-a1XtOIZgC6ZnsVeEf6DvMmc2R_StUi3JGEZ3hsS5OvQgH5kpkxQpAG0SSZgR&source=mofang&region=&source=mofang&aid=1180&region=' \
+curl --location --request POST 'https://webcast16-normal-useast5.us.tiktokv.com/webcast/room/mget_user_info?sec_user_ids=&source=mofang&region=&source=mofang&aid=1180&region=' \
 --header 'User-Agent: com.zhiliaoapp.musically/2022102050 (Linux; U; Android 7.1.2; en_US; Build/N2G48H;tt-ok/3.10.0.2)'
 
 this tiktok endpoint has no rate limit, you can check up to 51 users or, you can single check and hit up to 80krs!.
